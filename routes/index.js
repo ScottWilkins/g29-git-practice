35280bae4131d2434256f8bc5364f6de4aeb4a2e
Devin Stone <devstone11@gmail.com>
2016-07-20 10:12:15 -0600

    add dog route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/dog', function(req, res, next) {
+  res.send("woof I'm a dog");
 });
 
 module.exports = router;
